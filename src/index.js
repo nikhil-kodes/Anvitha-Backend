f153fc7aa698f73325d291f683626ca869aa9b34
NIKHIL SINGH <knikhilsingh01@gmail.com>
2025-09-05 00:42:41 +0530

    removed unnecessary files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/galleryroute.js";
-import { origin } from "./middlewares/origin.js";
 
 dotenv.config({
 	path: "./.env",
@@ -16,30 +15,28 @@ const PORT = process.env.PORT || 5000;
 // 	optionsSuccessStatus: 200
 //}
 
-const allowedOrigins = [
-	"https://anvithaclubglbajaj.netlify.app",
-	"http://localhost:8080",
-	"http://localhost:5173",
-	"http://172.19.80.1:8080",
-	"http://192.168.29.203:8080",
-];
+// const allowedOrigins = [
+// 	"https://anvithaclubglbajaj.netlify.app",
+// 	"http://localhost:8080",
+// 	"http://localhost:5173",
+// 	"http://172.19.80.1:8080",
+// 	"http://192.168.29.203:8080",
+// // ];
 
-const corsOptions = {
-	origin: (origin, callback) => {
-		if (!origin || allowedOrigins.includes(origin)) {
-			callback(null, true);
-		} else {
-			callback(new Error("Not allowed by CORS"));
-		}
-	},
-	optionsSuccessStatus: 200,
-};
+// const corsOptions = {
+// 	origin: (origin, callback) => {
+// 		if (!origin || allowedOrigins.includes(origin)) {
+// 			callback(null, true);
+// 		} else {
+// 			callback(new Error("Not allowed by CORS"));
+// 		}
+// 	},
+// 	optionsSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors({origin:"*"}));
 
 app.use(express.json());
-
-app.use(origin);
 
 app.use("/api/gallery", router);
 
